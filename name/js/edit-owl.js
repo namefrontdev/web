134dccd98ea78faa92b3af60839baf4041d4a939
namefrontdev <namefrontdev@gmail.com>
2018-08-25 19:49:22 +0700

    commit alphaV2

---
+++ name/js/edit-owl.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+ 
+    // $("#owl-example").owlCarousel({
+    //     navigation : true, 
+    //     slideSpeed : 300,
+    //     paginationSpeed : 400,
+    //     singleItem: true,
+    //     pagination: false,
+    //     rewindSpeed: 500
+    //   });
+    
+    $('#slider-area').owlCarousel({
+        loop:true,
+        autoplay: true,
+        margin:10,
+        nav:true,
+        smartSpeed: 1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+   
+  });
